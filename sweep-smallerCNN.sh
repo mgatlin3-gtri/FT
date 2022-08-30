41390ff89c3c57e3c476d18846600dc800ab1f31
Maia Gatlin <maia.gatlin@gtri.gatech.edu>
2022-08-29 21:01:29 -0400

    new sweep results + new sweep

---
+++ sweep-smallerCNN.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=smallCNN-sweep
+#SBATCH --output=/home/mgatlin3/FT/logs/out/%x_%j.out
+#SBATCH --error=/home/mgatlin3/FT/logs/err/%x_%j.err
+#SBATCH --export=NONE
+#SBATCH --mem=256G
+#SBATCH -n 3
+#SBATCH -N 3
+#SBATCH -t 4-12:00
+#SBATCH -c 32
+#SBATCH --gres=gpu:3 
+
+
+module load anaconda3
+module load gcc/9.2.0
+conda activate poop
+cd /home/mgatlin3/FT
+python sweep-smallerCNN.py
